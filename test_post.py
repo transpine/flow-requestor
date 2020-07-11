0ea0779e4de349b9019980aaf33aade441080426
JayD <jayd@trineedle.com>
2020-07-11 18:35:06 +0900

    add id num

--- test_post.py
+++ test_post.py
@@ -1,10 +1,15 @@
 import requests
+import datetime
 import json
 
 url_items = "http://localhost:3000/flows"
+getResponse = requests.get(url_items)
+id = len(getResponse.json())
+
 newItem = {
-    "time" : "test_time",
+    "id" : id,
+    "time" : datetime.datetime.now(),
     "event" : "test_event"
 }
 response = requests.post(url_items, data=newItem)
-print(response.text)
+# print(response.text)
